3a72b5067564a62342f424e4983ca43e21076108
Tangguhchandra <541231204@student.smktelkom-pwt.sch.id>
2024-11-19 08:49:31 +0700

    fillament

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Routes news
+Route::get('news', [NewsController::class, 'index']);
